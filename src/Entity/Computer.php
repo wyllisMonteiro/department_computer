54e2275a949f18b2390a2ccde3fc31b9f6f399df
wyllisMonteiro <wyllismonteiro@gmail.com>
2021-06-07 10:51:31 +0200

    feat: create entities

---
+++ src/Entity/Computer.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ComputerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ComputerRepository::class)
+ */
+class Computer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $systemName;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $purchase;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nameDepartment;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Department::class, inversedBy="computer")
+     */
+    private $department;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getSystemName(): ?string
+    {
+        return $this->systemName;
+    }
+
+    public function setSystemName(string $systemName): self
+    {
+        $this->systemName = $systemName;
+
+        return $this;
+    }
+
+    public function getPurchase(): ?\DateTimeInterface
+    {
+        return $this->purchase;
+    }
+
+    public function setPurchase(\DateTimeInterface $purchase): self
+    {
+        $this->purchase = $purchase;
+
+        return $this;
+    }
+
+    public function getNameDepartment(): ?string
+    {
+        return $this->nameDepartment;
+    }
+
+    public function setNameDepartment(string $nameDepartment): self
+    {
+        $this->nameDepartment = $nameDepartment;
+
+        return $this;
+    }
+
+    public function getDepartment(): ?Department
+    {
+        return $this->department;
+    }
+
+    public function setDepartment(?Department $department): self
+    {
+        $this->department = $department;
+
+        return $this;
+    }
+}
