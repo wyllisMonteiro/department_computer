14a887f05ce2b4f3b538424239a51e13f8fad527
wyllisMonteiro <wyllismonteiro@gmail.com>
2021-06-07 11:31:50 +0200

    feat: create list UI

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Department;
+use App\Repository\DepartmentRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -34,9 +35,32 @@ class DepartmentController extends AbstractController
             $connex = $this->getDoctrine()->getManager();
             $connex->persist($depart);
             $connex->flush();
-            return $this->render('department/list.html.twig');
+            return $this->redirectToRoute('department_list');
         } else {
             return $this->render('department/add.html.twig');
+        }
+    }
+
+    /**
+     * @Route("/departments", name="department_list")
+     */
+    public function GetDepartments(DepartmentRepository $repo, Request $req): Response
+    {
+        if ($req->isMethod('GET')) {
+            $departments = $repo->findAll();
+            return $this->render('department/list.html.twig', ['departments' => $departments]);
+        }
+    }
+
+    /**
+     * @Route("/departments/1/update", name="department_update")
+     */
+    public function UpdateDepartment(DepartmentRepository $repo, Request $req): Response
+    {
+        if ($req->isMethod('POST')) {
+            return new Response(1);
+        } else {
+            return new Response(1);
         }
     }
 }
