3b052ad5b063c9c7d1b37eae5953641f36f2b17c
wyllisMonteiro <wyllismonteiro@gmail.com>
2021-06-07 11:19:40 +0200

    feat: add department in db and show list UI

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Department;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DepartmentController extends AbstractController
 {
@@ -22,8 +24,19 @@ class DepartmentController extends AbstractController
     /**
      * @Route("/department/add", name="department_add")
      */
-    public function AddDepartment(): Response
+    public function AddDepartment(Request $req): Response
     {
-        return $this->render('department/add.html.twig');
+        if ($req->isMethod('POST')) {
+            $depart = new Department();
+            $depart->setName($req->get('department_name'));
+            $depart->setDomaine($req->get('department_domaine'));
+
+            $connex = $this->getDoctrine()->getManager();
+            $connex->persist($depart);
+            $connex->flush();
+            return $this->render('department/list.html.twig');
+        } else {
+            return $this->render('department/add.html.twig');
+        }
     }
 }
